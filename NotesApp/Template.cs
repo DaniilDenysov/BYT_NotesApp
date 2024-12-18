2c7f8fb897760f36707bc3645165e32cc9a76cb9
Александр Тарновский <aleks.tarnovskyi@gmail.com>
2024-12-18 09:50:21 +0100

    Added Template and noteTemplate

---
+++ NotesApp/Template.cs
@@ -0,0 +1,51 @@
+﻿namespace NotesApp;
+
+public class Templates : Category<TemplateNote>, IDisposable
+{
+    public string templateName { get; set; }
+    public List<TemplateNote> templateNotes { get; set; } =  new List<TemplateNote>();
+
+    public Templates()
+	{
+        ObjectManager.Instance.AddObject(this);
+    }
+
+    public void add(Templates templates)
+    {
+        if (templates == null)
+        {
+            throw new ArgumentNullException(nameof(templates));
+        }
+        ObjectManager.Instance.AddObject(templates);
+    }
+
+    public void Remove(Templates templates)
+    {
+        foreach (var templateNote in templates.templateNotes)
+        {
+            templateNote.Dispose();
+        }
+        ObjectManager.Instance.RemoveObj(templates);
+        GC.SuppressFinalize(templates);
+    }
+
+    public override int GetPriority()
+    {
+        return 1; 
+    }
+
+    public void Dispose()
+    {   
+        foreach (var templateNote in templateNotes)
+        {
+            templateNote.Dispose();
+        }
+        ObjectManager.Instance.RemoveObj(this);
+        GC.SuppressFinalize(this);
+    }
+
+    ~Templates()
+    {
+        ObjectManager.Instance.RemoveObj(this);
+    }
+}
---
+++ NotesApp/TemplateNote.cs
@@ -0,0 +1,47 @@
+﻿namespace NotesApp;
+using System;
+using System.ComponentModel;
+
+public class TemplateNote : Note , IDisposable
+{
+	public Templates template { get; set; }
+
+	public TemplateNote(Note note, Templates template) : base(note)
+    {
+		this.template = template;
+        template.templateNotes.Add(this);
+    }
+	
+	public void add (TemplateNote templateNote)
+	{ 
+		if(templateNote == null)
+		{
+            throw new ArgumentNullException(nameof(templateNote));
+        }
+
+		ObjectManager.Instance.AddObject(templateNote);
+    }
+
+	public void remove(TemplateNote templateNote)
+	{
+        if (templateNote == null)
+        {
+            throw new ArgumentNullException(nameof(templateNote));
+        }
+
+        template.templateNotes.Remove(templateNote);
+        ObjectManager.Instance.RemoveObj(templateNote);
+    }
+
+	public Note CreateNoteFromTemplate()
+	{
+		Note note = new Note(this.Title, this.Content);
+		return note;
+	}
+
+    ~TemplateNote()
+    {
+		template.templateNotes.Remove(this);
+        ObjectManager.Instance.RemoveObj(this);
+    }
+}
